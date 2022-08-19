d1838f2f511732a755326dc866230a399092d36a
DjonatasBorges <interdjonatas@gmail.com>
2022-08-19 08:08:02 -0300

    First commit. The implementation of the methods was performed. Tests run successfully.

--- src/main/java/com/sensedia/interview/services/impl/OrderBookServiceImpl.java
+++ src/main/java/com/sensedia/interview/services/impl/OrderBookServiceImpl.java
@@ -1,7 +1,9 @@
 package com.sensedia.interview.services.impl;
 
 import com.sensedia.interview.domain.Order;
+import com.sensedia.interview.domain.enumeration.Side;
 import com.sensedia.interview.services.OrderBookService;
+
 import java.util.List;
 import org.springframework.stereotype.Service;
 
@@ -10,23 +12,41 @@ public class OrderBookServiceImpl implements OrderBookService {
 
   @Override
   public long calculateTotalBuyOrders(List<Order> orderBook) {
-    return 0;
+    int totalBuyOrders = 0;
+    for (Order order : orderBook) {
+      if (order.getSide().equals(Side.BUY)){
+        totalBuyOrders += 1;
+      }
+    }
+    return totalBuyOrders;
   }
 
   @Override
   public long calculateTotalSellOrders(List<Order> orderBook) {
-    return 0;
+    int totalSellOrders = 0;
+    for (Order order : orderBook) {
+      if (order.getSide().equals(Side.SELL)){
+        totalSellOrders += 1;
+      }
+    }
+    return totalSellOrders;
   }
 
   @Override
-  public double calculateTotalValueOrders(
-      List<Order> orderBook) {
-    return 0;
+  public double calculateTotalValueOrders(List<Order> orderBook) {
+    double totalValueOrders = 0;
+    for (Order order : orderBook) {
+      totalValueOrders += order.getPrice();
+      }
+    return totalValueOrders;
   }
 
   @Override
-  public long calculateTotalQuantityOrders(
-      List<Order> orderBook) {
-    return 0;
+  public long calculateTotalQuantityOrders(List<Order> orderBook) {
+    int totalQuantifyOrders = 0;
+    for (Order order : orderBook) {
+      totalQuantifyOrders += order.getQuantity();
+    }
+    return totalQuantifyOrders;
   }
 }
